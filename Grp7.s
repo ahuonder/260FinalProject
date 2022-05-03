d192d78dc3130d7ed49d20df5678a0821f18e6c4
ahuonder <ahuonder28@gmail.com>
2022-05-02 17:47:51 -0700

    done

--- Grp7.s
+++ Grp7.s
@@ -1,46 +1,34 @@
-# test comments
+# Adam Huonder, Michael Bernard
+# CPSC 260
+# May 3rd, 2022
+# Final Project
 
 LC0:
-	.ascii "Enter a short sentence \0"
+	.ascii "Enter a short sentence \0" # user prompt
 LC1:
-	.ascii "%s\0"     # reads in a string with spaces until it hits an end line, or to read until end of line (\n)	
-LC4:
-	.ascii "%c\0"
-LC2:
-	.ascii "%c\12\0"
+	.ascii "%[^\n]\0"     # reads in a string with spaces until it hits an end line, or to read until end of line (\n)	
 
-	.globl	_main
+	.globl	_main										# initial setup
 	.def	_main;	.scl	2;	.type	32;	.endef
 
 _main:
 
 	pushl %ebp 				# \ Standard callee entrance
-	movl %esp, %ebp 		# /
+	movl %esp, %ebp 		# / preparing the stack
 	
 	subl	$128, %esp		# allocate memory to be used for the stack, stack size is 128 bytes
 
 	movl	$LC0, (%esp)	# moves prompt to the top of the stack
-	call	_printf  		# enter the sentence 
-
-	# movl 	$LC1, (%esp)	# moves the read in statement to the top of the stack
-	# call 	_scanf			# calls scan to read in the user input
+	call	_printf  		# print the prompt 
 
 	movl	$0, %esi		# initialize counter to zero
 
 GetInput:
 	# push the letters onto the stack
-	leal	8(%esp, %esi), %eax		# puts address of where the letter will go into eax
+	leal	8(%esp), %eax	# puts address of where the letters will go into eax
 	movl	%eax, 4(%esp)	# puts eax into stack for scanf param
-	movl	$LC1, (%esp)	# puts LC4 into stack for scanf param
-	call	_scanf
-	incl	%esi			# increments counter so mem address will be different and compare statement works
-FindNewLine:
-	# cmpl	$0x0a, (%eax)		# trying to compare the letter just scanned to see if its new line
-	# je		InitialLowerLoop
-C3:
-	# cmpl	$10, %esi		# assume max characters is 100 
-	# jle		GetInput		# jump back to get Input if not at 10 letters yet
-	# jmp		TestPrint
+	movl	$LC1, (%esp)	# puts LC1 into stack for scanf param
+	call	_scanf			# scan user input
 
 InitialLowerLoop:
 	movl	$0, %esi		# initialize the counter
@@ -55,7 +43,7 @@ FindLower2:
 	movb	%al, 8(%esp, %esi)	# move modified letter in 'a' register back to stack
 EndOfLoop:
 	incl	%esi			# increment loop counter
-	cmpb	$0, %al		# loop control comparison
+	cmpb	$0, %al			# loop control comparison
 	jne		FindLower1		# jump to top of loop
 
 	movl	$0, %esi		# reset/initialize loop counter
@@ -64,10 +52,10 @@ PrintOutput:
 	movb	%al, (%esp)		# move char in al to the top of the stack
 	call	_putchar		# print char at top of the stack
 	incl	%esi			# increment loop counter
-	cmpb	$0, %al		# loop control comparison
+	cmpb	$0, %al			# loop control comparison
 	jne		PrintOutput		# jump to top of print loop
 
-	leave
+	leave					# end program
 	ret
 
 	
