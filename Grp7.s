0c1d6bff54673bf504ef0faf2416b04aacf024cc
ahuonder <ahuonder28@gmail.com>
2022-05-02 17:51:43 -0700

    done + comments

--- Grp7.s
+++ Grp7.s
@@ -23,32 +23,31 @@ _main:
 
 	movl	$0, %esi		# initialize counter to zero
 
-GetInput:
-	# push the letters onto the stack
+GetInput: 				# push the letters onto the stack
 	leal	8(%esp), %eax	# puts address of where the letters will go into eax
 	movl	%eax, 4(%esp)	# puts eax into stack for scanf param
 	movl	$LC1, (%esp)	# puts LC1 into stack for scanf param
 	call	_scanf			# scan user input
 
-InitialLowerLoop:
+InitialLowerLoop:		# prepare to loop through user input
 	movl	$0, %esi		# initialize the counter
-FindLower1:
+FindLower1:				# checks lower bound of character
 	movb	8(%esp, %esi), %al	# move letter into eax
 	cmpb	$97, %al		# compare 97 (a) with eax value to see if lower case
 	jl		EndOfLoop		# if the value is less than 97, skip body and go to loop control
-FindLower2:
+FindLower2:				# checks upper bound of character, and changes if lower case
 	cmpb	$122, %al		# compare 122 (z) with eax value to see if lower case
 	jg		EndOfLoop		# if value is greater than 122, skip body and go to loop control
 	subb	$32, %al		# turn to upper case
 	movb	%al, 8(%esp, %esi)	# move modified letter in 'a' register back to stack
-EndOfLoop:
+EndOfLoop:				# loop control for FindLower1 and 2
 	incl	%esi			# increment loop counter
 	cmpb	$0, %al			# loop control comparison
 	jne		FindLower1		# jump to top of loop
 
 	movl	$0, %esi		# reset/initialize loop counter
-PrintOutput:
-	movb	8(%esp, %esi), %al	# move cahracter into al
+PrintOutput:			# prints the string in all capital letters
+	movb	8(%esp, %esi), %al	# move character into al
 	movb	%al, (%esp)		# move char in al to the top of the stack
 	call	_putchar		# print char at top of the stack
 	incl	%esi			# increment loop counter
